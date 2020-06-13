df323fc6a25f6f2b14106f93fe5f82689301abd4
ValVarius <60993964+ValVarius@users.noreply.github.com>
2020-06-13 10:10:39 -0700

    server test

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-const express = require ('express')
-const bodyParser = require('body-parser')
-const nodemailer = require('nodemailer')
-const app = express()
-const path = require('path');
-const PORT = process.env.PORT || 3001
-require('dotenv').config()
+const express = require("express");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+const app = express();
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+require("dotenv").config();
 console.log(process.env.PORT);
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false}))
+app.post("/api/form", (req, res) => {
+  console.log(req.body.mailState);
+  res.send(err);
 
-app.post('/api/form', (req,res) => {
-    console.log(req.body.mailState);
-
-    nodemailer.createTestAccount((err, account) => {
-        const htmlEmail = `
+  nodemailer.createTestAccount((err, account) => {
+    const htmlEmail = `
         <h3>Contact Details</h3>
         <ul>
         <li>Name: ${req.body.mailState.name}
@@ -23,61 +23,55 @@ app.post('/api/form', (req,res) => {
         </ul>
         <h3>Message</h3>
         <p>${req.body.mailState.message}</p>
-        `
+        `;
 
-        var transporter = nodemailer.createTransport({
-            service: 'gmail',
-            host: 'smtp.gmail.com',
-            auth: {
-              user:'valvarius1@gmail.com',
-              pass: process.env.PASSWORD
-            }
-          });
+    var transporter = nodemailer.createTransport({
+      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: "valvarius1@gmail.com",
+        pass: process.env.PASSWORD,
+      },
+    });
 
-          var mailOptions = {
-            from: 'valvarius1@gmail.com',
-            to: 'notitiami@gmail.com',
-            subject: 'Sending Email using Node.js[nodemailer]',
-            html: htmlEmail
-          };
+    var mailOptions = {
+      from: "valvarius1@gmail.com",
+      to: "notitiami@gmail.com",
+      subject: "Sending Email using Node.js[nodemailer]",
+      html: htmlEmail,
+    };
 
-        // const mailOptions = {
-        //     from: 'test@testaccount.com',
-        //     to:'carole.nicolas@ethereal.email',
-        //     replyTo: 'test@testaccount.com',
-        //     subject: 'New Message',
-        //     html: htmlEmail
-        // }
+    // const mailOptions = {
+    //     from: 'test@testaccount.com',
+    //     to:'carole.nicolas@ethereal.email',
+    //     replyTo: 'test@testaccount.com',
+    //     subject: 'New Message',
+    //     html: htmlEmail
+    // }
 
-        transporter.sendMail(mailOptions, (err, info) => {
-            if (err) {
-                return  res.send(err)
-                
-            }
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        return res.send(err);
+      }
 
-            console.log('Message sent: %s', info.accepted);
-            console.log('Message URL: %s', nodemailer.getTestMessageUrl(info));
-            
-            res.send(info)
-        })
+      console.log("Message sent: %s", info.accepted);
+      console.log("Message URL: %s", nodemailer.getTestMessageUrl(info));
 
+      res.send(info);
+    });
+  });
+});
 
-    })
-    
-})
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 
-
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static( 'client/build'));
-
-  app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname,'client', 'build', 'index.html'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
 
-
-app.listen(PORT, () =>{
-    console.log(`Server listening on port ${PORT}`);
-    
-})
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
