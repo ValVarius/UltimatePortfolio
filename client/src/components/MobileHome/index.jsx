8086804a8e7915ef8b0ab9a8d23b4efdf5ae0c0f
ValVarius <60993964+ValVarius@users.noreply.github.com>
2020-07-07 22:13:46 -0700

    fixed text prob with mobile home

--- client/src/components/MobileHome/index.jsx
+++ client/src/components/MobileHome/index.jsx
@@ -1,50 +1,47 @@
 import React from "react";
 import Zoom from "react-reveal/Zoom";
 import Rotate from "react-reveal/Rotate";
+
+
+
 import "./style.css";
 
 export default function MobileHome() {
   return (
-    <Zoom duration={2000}>
-    <div className="card homecard">
-      <div className="card-body">
-        {/*  SINGLE COLUMN  */}
-        <div className="col-12" id="title-column">
-          <h5 className="card-title homeCardTitle" id="mobile-card-title">
-            ~ About Me ~
-          </h5>
-          <hr />
-          <Zoom duration={3000}>
-            <img
-              className="img-fluid "
-              src={require("../../public/prof.jpg")}
-              alt="valerio varani"
-              id="profilePicture"
-            />
-          </Zoom>
-
-          <Rotate top right cascade>
-            <p className="card-text">Hello! </p>
-            <p className="card-text">
-              Born in Italy but spent most of my adult life abroad, I made the
-              U.S my home more than ten years ago.
-            </p>
-            <p className="card-text">
-              I have a degree in literature and one in cinema studies. I have
-              always had a curious mind which led me to a great variety of
-              experiences.
-            </p>
-            <p className="card-text">
-              I know multiple human and computer languages. I find myself most
-              motivated by novelty and learning
-            </p>
-            <p className="card-text">
-              I am a full-stack web developer!
-            </p>
+    <Zoom duration={1000}>
+      <div className="card homecard mobilehome">
+        <h5 className="card-title homeCardTitlemobile">~ About Me ~</h5>
+        <hr />
+        
+        <div className="card-body">
+        <Zoom duration={3000}>
+        <div className="col-12 center">
+        <img
+          className="card-img-top"
+          src={require("../../public/prof.jpg")}
+          alt="valerio varani"
+          id="profilePicture"
+        />
+        </div>
+        </Zoom>
+        <Rotate top right duration={5000} >
+          <p className="card-text">
+            Born in Italy but spent most of my adult life abroad, I made the U.S
+            my home more than ten years ago.
+          </p>
+          <p className="card-text">
+            I have a degree in literature and one in cinema studies. I have
+            always had a curious mind which led me to a great variety of
+            experiences.
+          </p>
+          <p className="card-text">
+            I know multiple human and computer languages. I find myself most
+            motivated by novelty and learning
+          </p>
+          <p className="card-text">I am a full-stack web developer!</p>
           </Rotate>
         </div>
       </div>
-    </div>
     </Zoom>
   );
 }
