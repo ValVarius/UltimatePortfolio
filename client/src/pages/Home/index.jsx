38482c3fa0094619d7a0d70fa411d78a3741325f
ValVarius <60993964+ValVarius@users.noreply.github.com>
2020-08-07 15:51:23 -0700

    improved contact page view for short height

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -12,53 +12,65 @@ import { useLocation } from "react-router-dom";
 export default function Home() {
   const location = useLocation();
 
-  // const [size, setSize] = useState({
-  //   width: window.innerWidth,
-  //   height: window.innerHeight
-  // });
-  // const updateSize = (event) => {
-  //   const { name, value } = event.target;
-  //   setSize({
-  //     ...size,
-  //     [name]: value,
-  //   })
-    
-  // };
-  // useEffect(() => {
-  //   window.addEventListener("resize", updateSize);
-  //   console.log(size.width,size.height);
-  //   console.log(window.innerWidth,window.innerHeight);
-  //   return () => window.removeEventListener("resize", updateSize);
-  // });
+  const [size, setSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+  const updateSize = () => {
+    // console.log(event.target.window);
+    // const { name, value } = event.target.window;
 
-  const [width, setWidth] = useState(window.innerWidth);
-  const updateWidth = () => {
-    setWidth(window.innerWidth);
+    setSize({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
   };
   useEffect(() => {
-    window.addEventListener("resize", updateWidth);
-    return () => window.removeEventListener("resize", updateWidth);
+    window.addEventListener("resize", updateSize);
+    console.log(size.width, size.height);
+    console.log(window.innerWidth, window.innerHeight);
+    return () => window.removeEventListener("resize", updateSize);
   });
 
+  // const [width, setWidth] = useState(window.innerWidth);
+  // const updateWidth = () => {
+  //   setWidth(window.innerWidth);
+  // };
+  // useEffect(() => {
+  //   window.addEventListener("resize", updateWidth);
+  //   return () => window.removeEventListener("resize", updateWidth);
+  // });
 
   return (
-    <div className="container-fluid" id = "homecontainer">
-      {location.pathname === "/" && width >= 502 ? <HomeCard /> : ""}
-      {location.pathname === "/" && width < 502 ? <MobileHome /> : ""}
-      {location.pathname === "/portfolio" && width >= 1100 ? (
-        <Portfolio/>
+    <div className="container-fluid" id="homecontainer">
+      {location.pathname === "/" && size.width >= 502 ? <HomeCard /> : ""}
+      {location.pathname === "/" && size.width < 502 ? <MobileHome /> : ""}
+      {location.pathname === "/portfolio" && size.width >= 1100 ? (
+        <Portfolio />
       ) : (
         ""
       )}
-      {location.pathname === "/portfolio" && width < 1100 ? (
+      {location.pathname === "/portfolio" && size.width < 1100 ? (
         <Portfolio />
       ) : (
         ""
       )}
 
-      {location.pathname === "/contacts" && width >= 520 ? <ContactCard /> : ""}
-      {location.pathname === "/contacts" && width < 520 ? <MobileContactCard /> : ""}
-      {/* {location.pathname === "/contacts" && size.heigth <= 620 ? <MobileContactCard /> : ""} */}
+      {location.pathname === "/contacts" && size.width >= 520 ? (
+        <ContactCard />
+      ) : (
+        ""
+      )}
+      {location.pathname === "/contacts" && size.width < 520 ? (
+        <MobileContactCard/>
+      ) : (
+        ""
+      )}
+      {/* {location.pathname === "/contacts" && size.heigth <= 620 ? (
+        <Portfolio />
+      ) : (
+        ""
+      )} */}
     </div>
   );
 }
