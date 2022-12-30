7371e5ca848e40eede77599f1f824c95f495c5b3
ValVarius <60993964+ValVarius@users.noreply.github.com>
2022-12-30 01:24:40 -0700

    fixing iphone bug on homepage

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -8,21 +8,6 @@ import "./style.css";
 export default function Home() {
   const location = useLocation();
 
-  const [size, setSize] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
-  });
-  const updateSize = () => {
-    setSize({
-      width: window.innerWidth,
-      height: window.innerHeight,
-    });
-  };
-  useEffect(() => {
-    window.addEventListener("resize", updateSize);
-    return () => window.removeEventListener("resize", updateSize);
-  });
-
   return (
     <div id="homecontainer">
       {location.pathname === "/" ? <HomePage /> : ""}
